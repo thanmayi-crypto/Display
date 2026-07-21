07571293aae69d2cc1775d473a27e1e718316a59
thanmayi-crypto <venkatapadmajaduvvuru@gmail.com>
2026-07-21 20:49:50 +0530

    Delete Hello.java

--- Hello.java
+++
@@ -1,6 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Jenkins!");
-        System.out.println("My Name is Ragathanmayi elletla 23mic7166");
-    }
-}
