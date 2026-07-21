b2dc8d63425ea56eeacf282856868d4f27039559
thanmayi-crypto <venkatapadmajaduvvuru@gmail.com>
2026-07-21 20:49:31 +0530

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo Jenkins Multi-Configuration Project
+
+echo Hello Ragathanmayi elletla 23mic7166!
+echo Welcome to Jenkins.
+
+echo.
+
+echo Current Environment : %ENV%
+echo Computer Name       : %COMPUTERNAME%
+echo User Name           : %USERNAME%
+
+echo.
+
+echo Build for %ENV% completed successfully.
